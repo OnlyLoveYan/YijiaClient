d425004a1b8b4b52aed89a59ccbebbfe11a9ab74
love言 <2663044650@qq.com>
2024-11-13 10:59:23 +0800

    test

--- src/main/java/com/yijia/Main.java
+++ src/main/java/com/yijia/Main.java
@@ -1,12 +1,11 @@
 package com.yijia;
 
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.yijia.service.impl.MqttServiceImpl;
 
-@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        MqttServiceImpl mqttService = new MqttServiceImpl();
+        mqttService.publish("","");
     }
 }
--- src/main/java/com/yijia/callback/PushCallback.java
+++ src/main/java/com/yijia/callback/PushCallback.java
@@ -1,14 +1,12 @@
 package com.yijia.callback;
 
 
-import lombok.extern.slf4j.Slf4j;
+
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.springframework.stereotype.Component;
 
-@Component
-@Slf4j
+
 public class PushCallback implements MqttCallback {
 
 
@@ -19,7 +17,6 @@ public class PushCallback implements MqttCallback {
 
     @Override
     public void messageArrived(String topic, MqttMessage message) throws Exception {
-        log.info("接收到消息：{}，主题：{}", new String(message.getPayload()), topic);
     }
 
     @Override
